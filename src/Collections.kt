1fd2df8ae9c7cf945da9c696e41e1fb96702a230
Max <maxib2007@gmail.com>
2021-02-03 17:26:34 -0300

    collections

---
+++ src/Collections.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val imperials = listOf("Emperor", "Darth Vader", "Boba Fett", "Tarkin")
+    println(imperials.sorted())
+    println(imperials[0])
+    println(imperials.last())
+    println(imperials.contains("Emperor"))
+    println(imperials)
+
+    val rebels = arrayListOf("Leiah", "Luke", "Han Solo", "Mon Mothma")
+    println(rebels.size)
+    rebels.add("Chewbacca")
+    println(rebels)
+    rebels.add(0, "Lando")
+    println(rebels)
+    println(rebels.indexOf("Luke"))
+    rebels.remove("Han Solo")
+    println(rebels)
+
+    val rebelVehiclesMap = mapOf("Solo" to "Millenium Falcon", "Luke" to "Landspeeder")
+    println(rebelVehiclesMap["Solo"])
+    println(rebelVehiclesMap.get("Solo"))
+    println(rebelVehiclesMap.getOrDefault("Leiah", "This ship doesnt exist"))
+    println(rebelVehiclesMap.values)
+
+    val rebelVehicles = hashMapOf("Solo" to "Millenium Falcon", "Luke" to "Landspeeder", "Boba Fett" to "Rocket Pack")
+    rebelVehicles["Luke"] = "XWing"
+    rebelVehicles.put("Leiah", "Tantive IV")
+    println(rebelVehicles)
+    rebelVehicles.remove("Boba Fett")
+    println(rebelVehicles)
+    rebelVehicles.clear()
+    println(rebelVehicles)
+}
