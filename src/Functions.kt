ff5014660e638722038e8be24e392f2f66cc6bd8
Max <maxib2007@gmail.com>
2021-02-03 16:56:33 -0300

    functions

---
+++ src/Functions.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    fun startRun() {
+        println("Is running")
+    }
+
+    fun sayHi(name: String) {
+        println("Hi $name!")
+    }
+
+    fun calculateNum(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+
+    fun sayMyName(name: String = "Tomas") {
+        println("My name is $name")
+    }
+
+    sayHi("Maxwell")
+    startRun()
+    println(calculateNum(1, 2))
+    sayMyName()
+    sayMyName("Max")
+}
