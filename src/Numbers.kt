751c68f503844b9cbaf24a2c31ae804ddcfb4dc1
Max <maxib2007@gmail.com>
2021-02-03 16:47:59 -0300

    operators

---
+++ src/Numbers.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a = 10
+    val b = 2
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+
+    if(a % b == 0) {
+        println("Is even")
+    } else {
+        println("Is odd")
+    }
+}
