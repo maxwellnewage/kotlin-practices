23344b7ab0a9b870b9fa7bea4335880142b71718
Max <maxib2007@gmail.com>
2021-02-11 11:40:18 -0300

    some changes on loop examples

--- src/main/kotlin/examples/Loops.kt
+++ src/main/kotlin/examples/Loops.kt
@@ -1,6 +1,49 @@
 package examples
 
 fun main() {
+//    rebelsDemo()
+//    whileDemo()
+    forDemo()
+}
+
+fun forDemo() {
+    // 0 to 10
+    for(i in 0..10) {
+        println("Number is $i")
+    }
+
+    println("<=====================>")
+
+    // 0 to 9
+    for (i in 0 until 10) {
+        println("Number is $i")
+    }
+
+    println("<=====================>")
+
+    // 10 to 0
+    for (i in 10 downTo 0) {
+        println("Number is $i")
+    }
+
+    println("<=====================>")
+
+    // 0 to 10 but only pairs numbers
+    for (i in 0..10 step 2) {
+        println("Number is $i")
+    }
+}
+
+fun whileDemo() {
+    var x = 10
+
+    while (x > 0) {
+        println(x)
+        x--
+    }
+}
+
+fun rebelsDemo() {
     val rebels = arrayListOf("Leiah", "Luke", "Han Solo", "Mon Mothma")
 
     val rebelVehicles = hashMapOf("Solo" to "Millenium Falcon", "Luke" to "Landspeeder", "Boba Fett" to "Rocket Pack")
@@ -11,12 +54,5 @@ fun main() {
 
     for ((character, vehicle) in rebelVehicles) {
         println("$character gets around in their $vehicle")
-    }
-
-    var x = 10
-
-    while (x > 0) {
-        println(x)
-        x--
     }
 }
