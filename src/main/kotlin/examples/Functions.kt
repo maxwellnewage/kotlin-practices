1dd6b55b47062e662dc26592a55e0a7f4e74e1cc
Max <maxib2007@gmail.com>
2021-02-13 13:19:45 -0300

    refactor private functions on functions demo

--- src/main/kotlin/examples/Functions.kt
+++ src/main/kotlin/examples/Functions.kt
@@ -1,26 +1,25 @@
 package examples
 
 fun main() {
-
-    fun startRun() {
-        println("Is running")
-    }
-
-    fun sayHi(name: String) {
-        println("Hi $name!")
-    }
-
-    fun calculateNum(num1: Int, num2: Int): Int {
-        return num1 + num2
-    }
-
-    fun sayMyName(name: String = "Tomas") {
-        println("My name is $name")
-    }
-
     sayHi("Maxwell")
     startRun()
     println(calculateNum(1, 2))
     sayMyName()
     sayMyName("Max")
+}
+
+private fun startRun() {
+    println("Is running")
+}
+
+private fun sayHi(name: String) {
+    println("Hi $name!")
+}
+
+private fun calculateNum(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+private fun sayMyName(name: String = "Tomas") {
+    println("My name is $name")
 }
