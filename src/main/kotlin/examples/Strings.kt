cba47241990db79215304b8f7e18c920d0b6ba58
Max <maxib2007@gmail.com>
2021-02-13 13:36:50 -0300

    refactor strings demo

--- src/main/kotlin/examples/Strings.kt
+++ src/main/kotlin/examples/Strings.kt
@@ -4,36 +4,40 @@ fun main() {
     val str = "This is a\ngreat string."
     println(str)
 
-    val scapeChar = "This is a \"quote\""
-    println(scapeChar)
+    println("This is a \"quote\"")
+    println("This is \$1000")
 
-    val dolarSign = "This is \$1000"
-    println(dolarSign)
+    findInString(str)
 
+    println(str.toLowerCase())
+    println(str.toUpperCase())
+
+    numToString()
+
+    println(str.subSequence(0, 4))
+
+    stringMultipleLines()
+}
+
+private fun stringMultipleLines() {
     val rawCrawl = """
         |A long time ago,
         |in a galaxy
         |far, far, away...""".trimMargin()
 
     println(rawCrawl)
+}
 
-//    for(char in str) {
-//        println(char)
-//    }
+private fun numToString() {
+    val num = 6
+    val stringNum = num.toString()
+    println(stringNum)
+}
 
+private fun findInString(str: String) {
     val contentEquals = str.contentEquals("great")
     println(contentEquals)
 
     val contains = str.contains("great")
     println(contains)
-
-    println(str.toLowerCase())
-    println(str.toUpperCase())
-
-    val num = 6
-    val stringNum = num.toString()
-    println(stringNum)
-
-    val subsequence = str.subSequence(0, 4)
-    println(subsequence)
 }
