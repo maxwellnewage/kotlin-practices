8008a65ed0b832cf2adf3f4563f530b1b6665dce
Max <maxib2007@gmail.com>
2021-02-13 13:27:29 -0300

    IntArrraySum demo to functions

--- src/main/kotlin/examples/IntArraySum.kt
+++ src/main/kotlin/examples/IntArraySum.kt
@@ -3,6 +3,22 @@ package examples
 import models.Person
 
 fun main() {
+    lambdaForEachDemo()
+    sumByDemo()
+}
+
+private fun sumByDemo() {
+    // You can also sum up properties of collections of objects by using sumBy:
+    val theDoeFamily = listOf(
+        Person("John", "Doe", 30),
+        Person("Jane", "Doe", 31),
+        Person("Junior", "Doe", 2),
+    )
+    val totalAge = theDoeFamily.sumBy { it.age }
+    println("The total of the Doe family's ages is $totalAge")
+}
+
+private fun lambdaForEachDemo() {
     val arr: IntArray = intArrayOf(1,1,1)
     var sumA = 0
 
@@ -12,13 +28,4 @@ fun main() {
 
     // Alternatively:
     println(arr.sum())
-
-    // You can also sum up properties of collections of objects by using sumBy:
-    val theDoeFamily = listOf(
-        Person("John", "Doe", 30),
-        Person("Jane", "Doe", 31),
-        Person("Junior", "Doe", 2),
-    )
-    val totalAge = theDoeFamily.sumBy { it.age }
-    println("The total of the Doe family's ages is $totalAge")
 }
