6a7bde85785f0835483907438f8ffaf485a73061
Max <maxib2007@gmail.com>
2021-02-13 13:14:15 -0300

    refactor classes demo to functions

--- src/main/kotlin/examples/Classes.kt
+++ src/main/kotlin/examples/Classes.kt
@@ -6,6 +6,24 @@ import models.Person
 import models.Truck
 
 fun main() {
+//    classesDemo()
+//    compontentButtonDemo()
+//    SAMDemo()
+
+    dataClassDemo()
+}
+
+fun dataClassDemo() {
+    // Classes that are just meant to hold data can be defined as "data classes". They get a copy() function
+    // plus toString() and other niceties.
+    val johnDoe = Person("John", "Doe", 35)
+    println(johnDoe)
+
+    val olderJohnDoe = johnDoe.copy(age = 36)
+    println("It's John's birthday! He used to be $johnDoe but is now $olderJohnDoe!")
+}
+
+fun classesDemo() {
     val tesla = Car("Tesla", "A-45", "Red")
     tesla.move()
     tesla.accelerate()
@@ -13,7 +31,9 @@ fun main() {
 
     val truck = Truck("Toyota", "Avalon")
     truck.accelerate()
+}
 
+fun compontentButtonDemo() {
     val okButton = Button("OK")
 
     okButton.setOnClickListener(object: Button.OnClickListener {
@@ -22,22 +42,16 @@ fun main() {
         }
     })
 
+    okButton.press()
+}
+
+fun SAMDemo() {
     // syntax for SAM interfaces is only possible when the interface is written in Java
     // or if using Kotlin 1.4+ the interface must be defined as a "fun interface"
     //
     // https://stackoverflow.com/questions/56557625/how-can-i-call-an-interface-in-kotlin
     // https://kotlinlang.org/docs/reference/whatsnew14.html#sam-conversions-for-kotlin-interfaces
-//    okButton.setOnClickListener {
-//        println("this is a click action!")
-//    }
-
-    okButton.press()
-
-    // Classes that are just meant to hold data can be defined as "data classes". They get a copy() function
-    // plus toString() and other niceties.
-    val johnDoe = Person("John", "Doe", 35)
-    println(johnDoe)
-
-    val olderJohnDoe = johnDoe.copy(age = 36)
-    println("It's John's birthday! He used to be $johnDoe but is now $olderJohnDoe!")
+    //    okButton.setOnClickListener {
+    //        println("this is a click action!")
+    //    }
 }
