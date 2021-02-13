8609bf90823093de020d3632a185ac6b6b154559
Max <maxib2007@gmail.com>
2021-02-13 13:32:14 -0300

    math refactor

--- src/main/kotlin/examples/Math.kt
+++ src/main/kotlin/examples/Math.kt
@@ -3,18 +3,12 @@ package examples
 import kotlin.math.*
 
 fun main() {
-    // Exponent operator
-    //a^b
-    val a: Double = 3.0
-    val b = 2
+    exponentOperator()
+    calculateAreaOfCircle()
+    sinOfNumber()
+}
 
-    println("$a^$b: ${a.pow(b)}")
-
-    // Area of a circle with radius r
-    val r = 2.3
-    println("Area: ${PI * r.pow(2)}")
-
-
+private fun sinOfNumber() {
     var x: Double = 1.0/2
     var cal = sin(x * PI)
     println("sin of $x*pi = ${round(cal)}")
@@ -22,6 +16,19 @@ fun main() {
     x = 3.0/2
     cal = sin(x * PI)
     println("sin of $x*pi = ${round(cal)}")
+}
 
+private fun exponentOperator() {
+    // Exponent operator
+    //a^b
+    val a: Double = 3.0
+    val b = 2
 
+    println("$a^$b: ${a.pow(b)}")
+}
+
+private fun calculateAreaOfCircle() {
+    // Area of a circle with radius r
+    val r = 2.3
+    println("Area: ${PI * r.pow(2)}")
 }
